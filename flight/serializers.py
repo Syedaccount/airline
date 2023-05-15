307b5f184c3fdbb71eb8aaeca3a143c26936887f
syed yousaf <syedyousaf98@gmail.com>
2023-05-15 23:36:35 +0500

    create FlightSerializer for Flight model

--- flight/serializers.py
+++ flight/serializers.py
@@ -1,3 +1,8 @@
 from rest_framework import serializers
 from .models import *
 
+
+class FlightSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ["origin", "destination", "duration"]
+        model = Flight
