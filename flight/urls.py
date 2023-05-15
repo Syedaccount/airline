286125e0cc65e68e1fec50f17f0007e8742c1e16
syed yousaf <syedyousaf98@gmail.com>
2023-05-15 23:43:28 +0500

    setup url configuration with router class that come up with django rest framework

--- flight/urls.py
+++ flight/urls.py
@@ -1,9 +1,14 @@
-
+from rest_framework import routers
+from django.urls import path, include
 from . import views
 
 
 app_name = "flight"
 
+router = routers.DefaultRouter()
+
+router.register("flights", views.FlightViewSet, basename="flights")
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", include(router.urls)),
 ]
