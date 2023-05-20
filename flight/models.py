df0b633c5c84742ca602d21a2c20799f4a2226cf
syed yousaf <syedyousaf98@gmail.com>
2023-05-20 13:51:32 +0500

    alter __str__ method

--- flight/models.py
+++ flight/models.py
@@ -35,4 +35,4 @@ class Passenger(models.Model):
     flight = models.ManyToManyField(Flight, blank=True, related_name="passengers")
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.user.first_name} {self.user.last_name}"
